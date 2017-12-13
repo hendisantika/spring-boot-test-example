10e7abbb0e3f01f78756d4b7680985b146f9d886
Hendi Santika <hendisantika@gmail.com>
2017-12-13 07:58:21 +0700

    #26 - Add Main Spring Boot Service Test

--- src/test/java/com/hendisantika/springboottestexample/SpringBootTestExampleApplicationTests.java
+++ src/test/java/com/hendisantika/springboottestexample/SpringBootTestExampleApplicationTests.java
@@ -1,16 +1,44 @@
 package com.hendisantika.springboottestexample;
 
+import com.hendisantika.springboottestexample.domain.VehicleIdentificationNumber;
+import com.hendisantika.springboottestexample.service.VehicleDetails;
+import com.hendisantika.springboottestexample.service.VehicleDetailsService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.BDDMockito.given;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
 public class SpringBootTestExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private static final VehicleIdentificationNumber VIN = new VehicleIdentificationNumber(
+            "01234567890123456");
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @MockBean
+    private VehicleDetailsService vehicleDetailsService;
+
+    @Before
+    public void setup() {
+        given(this.vehicleDetailsService.getVehicleDetails(VIN))
+                .willReturn(new VehicleDetails("Honda", "Civic"));
+    }
+
+    @Test
+    public void test() {
+        this.restTemplate.getForEntity("/{username}/vehicle", String.class, "sframework");
+    }
+
 
 }
