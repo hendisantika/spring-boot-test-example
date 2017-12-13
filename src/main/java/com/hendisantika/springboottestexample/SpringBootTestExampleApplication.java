609b7115398db00f539f0edb0599686386100663
Hendi Santika <hendisantika@gmail.com>
2017-12-13 07:11:05 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboottestexample/SpringBootTestExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springboottestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestExampleApplication.class, args);
+	}
+}
