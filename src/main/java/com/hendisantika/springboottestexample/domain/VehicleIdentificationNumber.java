2db8c3c190b0b2807050f108eda5b8f466d2e2f8
Hendi Santika <hendisantika@gmail.com>
2017-12-13 07:13:45 +0700

    #3 - Vehicle Identification Number

---
+++ src/main/java/com/hendisantika/springboottestexample/domain/VehicleIdentificationNumber.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springboottestexample.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/12/17
+ * Time: 07.13
+ * To change this template use File | Settings | File Templates.
+ */
+public class VehicleIdentificationNumber {
+}
